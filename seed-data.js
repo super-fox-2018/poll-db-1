6819e44dd5e0855cb05d4dd7f077cb444f74926a
ariefardi <forzaariefpyon@gmail.com>
2018-05-14 14:55:43 +0700

    selesai crud

--- seed-data.js
+++ seed-data.js
@@ -30,23 +30,28 @@ function readFile(file,cb){
 // 		console.log(split)
 		
 // })
-readFile('./voters.csv',(data)=>{
-	let splitData = ''
-	let split = []
-		console.log('membaca data')
-		for(var i=0;i<data.length;i++){
-			splitData = data.split("\n")
-		}
-		for(var i=0;i<splitData.length;i++){
-			split.push(splitData[i].split(','))
-		}
-		// for(var i=0;i<split.length;i++){
-		// 		db.run(`INSERT INTO Voters (id,first_name,last_name,gender,age)
-		// 				VALUES (null,"${split[i][0]}","${split[i][1]}","${split[i][2]}",${split[i][3]})`)
-		// }
-		console.log(split)
+// readFile('./voters.csv',(data)=>{
+// 	let splitData = ''
+// 	let split = []
+// 		console.log('membaca data')
+// 		for(var i=0;i<data.length;i++){
+// 			splitData = data.split("\n")
+// 		}
+// 		for(var i=1;i<splitData.length;i++){
+// 			split.push(splitData[i].split(','))
+// 		}
+// 		for(var i=0;i<split.length;i++){
+// 				db.run(`INSERT INTO Voters (id,first_name,last_name,gender,age)
+// 						VALUES (null,"${split[i][0]}","${split[i][1]}","${split[i][2]}","${split[i][3]}")`,function(err){
+// 							if (err) {throw err}
+// 								else{
+// 									console.log('selesai')
+// 								}
+// 						})
+// 		}
+// 		//console.log(split)
 		
-})
+// })
 // readFile('./votes.csv',(data)=>{
 // 	let splitData = ''
 // 	let split = []
@@ -54,12 +59,17 @@ readFile('./voters.csv',(data)=>{
 // 		for(var i=0;i<data.length;i++){
 // 			splitData = data.split("\n")
 // 		}
-// 		for(var i=0;i<splitData.length;i++){
+// 		for(var i=1;i<splitData.length;i++){
 // 			split.push(splitData[i].split(','))
 // 		}
 // 		for(var i=0;i<split.length;i++){
 // 				db.run(`INSERT INTO Votes (id,voterId,politicianId)
-// 						VALUES (null,${split[i][0]},${split[i][1]})`)
+// 						VALUES (null,"${split[i][0]}","${split[i][1]}")`,function(err){
+// 							if (err) {throw err}
+// 								else{
+// 									console.log('seeding data')
+// 								}
+// 						})
 // 		}
 // 		//console.log(split)
 		
