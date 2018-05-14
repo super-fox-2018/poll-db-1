65d01ec3327c14e395a523b31031113b573234a7
Rin Ng <Rin08.ng@gmail.com>
2018-05-14 16:22:35 +0700

    Added code to setup.js to call query from terminal and commented out codes that duplicates data save to db

--- seed-data.js
+++ seed-data.js
@@ -1,44 +1,47 @@
+
+const fs = require("fs")
+
 const sqlite3  = require('sqlite3').verbose();
 const db       = new sqlite3.Database('./poll.db');
 
-
-//Adding data from csv files to database
-db.serialize (function politician(){
-  var stmt = db.prepare("INSERT INTO politicians(name, party, location, grade_current) VALUES (?,?,?,?)");
-  let politicians = fs.readFileSync("politicians.csv", "utf8").split("\n");
-  politicians.splice(0,1);
-
-  for (let i = 0; i < politicians.length; i++){
-    politicians[i] = politicians[i].split(",");
-    stmt.run(`${politicians[i][0]}`, `${politicians[i][1]}`, `${politicians[i][2]}`, `${politicians[i][3]}`);
-    }
-  stmt.finalize();
-
-});
-
-db.serialize (function votes(){
-  var stmt = db.prepare("INSERT INTO votes(voter_id, politician_id) VALUES (?,?)");
-  let votes = fs.readFileSync("votes.csv", "utf8").split("\n");
-  votes.splice(0,1);
-  for (let i = 0; i < votes.length; i++){
-    votes[i] = votes[i].split(",");
-    stmt.run(`${votes[i][0]}`, `${votes[i][1]}`);
-    }
-  stmt.finalize();
-
-});
-
-db.serialize (function voters(){
-  var stmt = db.prepare("INSERT INTO voters(first_name, last_name, gender, age) VALUES (?,?,?,?)");
-  let voters = fs.readFileSync("voters.csv", "utf8").split("\n");
-  voters.splice(0,1);
-  for (let i = 0; i < voters.length; i++){
-    voters[i] = voters[i].split(",");
-    stmt.run(`${voters[i][0]}`, `${voters[i][1]}`, `${voters[i][2]}`, `${voters[i][3]}`);
-    }
-  stmt.finalize();
-
-});
+// 
+// //Adding data from csv files to database
+// db.serialize (function politician(){
+//   var stmt = db.prepare("INSERT INTO politicians(name, party, location, grade_current) VALUES (?,?,?,?)");
+//   let politicians = fs.readFileSync("politicians.csv", "utf8").split("\n");
+//   politicians.splice(0,1);
+//
+//   for (let i = 0; i < politicians.length; i++){
+//     politicians[i] = politicians[i].split(",");
+//     stmt.run(`${politicians[i][0]}`, `${politicians[i][1]}`, `${politicians[i][2]}`, `${politicians[i][3]}`);
+//     }
+//   stmt.finalize();
+//
+// });
+//
+// db.serialize (function votes(){
+//   var stmt = db.prepare("INSERT INTO votes(voter_id, politician_id) VALUES (?,?)");
+//   let votes = fs.readFileSync("votes.csv", "utf8").split("\n");
+//   votes.splice(0,1);
+//   for (let i = 0; i < votes.length; i++){
+//     votes[i] = votes[i].split(",");
+//     stmt.run(`${votes[i][0]}`, `${votes[i][1]}`);
+//     }
+//   stmt.finalize();
+//
+// });
+//
+// db.serialize (function voters(){
+//   var stmt = db.prepare("INSERT INTO voters(first_name, last_name, gender, age) VALUES (?,?,?,?)");
+//   let voters = fs.readFileSync("voters.csv", "utf8").split("\n");
+//   voters.splice(0,1);
+//   for (let i = 0; i < voters.length; i++){
+//     voters[i] = voters[i].split(",");
+//     stmt.run(`${voters[i][0]}`, `${voters[i][1]}`, `${voters[i][2]}`, `${voters[i][3]}`);
+//     }
+//   stmt.finalize();
+//
+// });
 
 
 
--- setup.js
+++ setup.js
@@ -1,92 +1,163 @@
 //your code here
-const fs = require("fs")
 const db    = require('./seed-data.js');
 
 
-//Insert, Update and Delete data into/from DB
+Insert, Update and Delete data into/from DB
 
 const args  = process.argv.slice(2);
-let userInput = args[1];
-let query;
-if(args[0].toUpperCase() === "INSERT"){
-  switch (userInput){
-    case "politicians" : {
-      query = `INSERT INTO politicians (name, party, location, grade_current)
-               VALUES ('${args[2]}', '${args[3]}', '${args[4]}', '${args[5]}')`;
-      break;
-    }
-    case "voters" : {
-      query = `INSERT INTO voters (first_name, last_name, gender, age)
-               VALUES ('${args[2]}', '${args[3]}', '${args[4]}', '${args[5]}')`;
-      break;
-    }
-    case "votes" : {
-      query = `INSERT INTO votes (voter_id, politician_id)
-               VALUES ('${args[2]}', '${args[3]}')`;
-      break;
-    }
-  }
-}
-else if(args[0].toUpperCase() === "UPDATE"){
-  switch(userInput){
-    case "politicians" : {
-      query =  `UPDATE politicians
-                SET name = '${args[2]}'',
-                    party = '${args[3]}',
-                    location = '${args[4]}',
-                    grade_current = '${args[5]}'
-                WHERE
-                    politician_id = ${args[6]}`;
-
-      break;
-    }
-    case "voters" : {
-      query =  `UPDATE voters
-                SET first_name = '${args[2]}',
-                    last_name = '${args[3]}',
-                    gender = '${args[4]}',
-                    age = '${args[5]}'
-                WHERE
-                    voter_id = ${args[6]}`;
-      break;
-    }
-    case "votes" : {
-      query = `UPDATE voters
-                SET voter_id = '${args[2]}',
-                    politician_id = '${args[3]}'`;
-      break;
+if(args[1] !== undefined){
+ let userInput = args[1];
+  let query;
+  if(args[0].toUpperCase() === "INSERT"){
+    switch (userInput){
+      case "politicians" : {
+        query = `INSERT INTO politicians (name, party, location, grade_current)
+                 VALUES ('${args[2]}', '${args[3]}', '${args[4]}', '${args[5]}')`;
+        break;
+      }
+      case "voters" : {
+        query = `INSERT INTO voters (first_name, last_name, gender, age)
+                 VALUES ('${args[2]}', '${args[3]}', '${args[4]}', '${args[5]}')`;
+        break;
+      }
+      case "votes" : {
+        query = `INSERT INTO votes (voter_id, politician_id)
+                 VALUES ('${args[2]}', '${args[3]}')`;
+        break;
+      }
     }
   }
-}
+  else if(args[0].toUpperCase() === "UPDATE"){
+    switch(userInput){
+      case "politicians" : {
+        query =  `UPDATE politicians
+                  SET name = '${args[2]}'',
+                      party = '${args[3]}',
+                      location = '${args[4]}',
+                      grade_current = '${args[5]}'
+                  WHERE
+                      politician_id = ${args[6]}`;
 
-else if(args[0].toUpperCase()  === "DELETE"){
-  switch(userInput){
-    case "politicians" : {
-      query =  `DELETE FROM politicians
-                WHERE
-                politician_id = '${args[2]}'`;
-
-      break;
-    }
-    case "voters" : {
-      query =  `DELETE FROM voters
-                WHERE
-                voter_id = '${args[2]}'`;
-      break;
-    }
-    case "votes" : {
-      query = `DELETE FROM votes
-              WHERE
-              id = '${args[2]}'`;
-      break;
+        break;
+      }
+      case "voters" : {
+        query =  `UPDATE voters
+                  SET first_name = '${args[2]}',
+                      last_name = '${args[3]}',
+                      gender = '${args[4]}',
+                      age = '${args[5]}'
+                  WHERE
+                      voter_id = ${args[6]}`;
+        break;
+      }
+      case "votes" : {
+        query = `UPDATE voters
+                  SET voter_id = '${args[2]}',
+                      politician_id = '${args[3]}'`;
+        break;
+      }
     }
   }
+
+  else if(args[0].toUpperCase()  === "DELETE"){
+    switch(userInput){
+      case "politicians" : {
+        query =  `DELETE FROM politicians
+                  WHERE
+                  politician_id = '${args[2]}'`;
+
+        break;
+      }
+      case "voters" : {
+        query =  `DELETE FROM voters
+                  WHERE
+                  voter_id = '${args[2]}'`;
+        break;
+      }
+      case "votes" : {
+        query = `DELETE FROM votes
+                WHERE
+                id = '${args[2]}'`;
+        break;
+      }
+    }
+  }
+
+  db.run(query, function (err) {
+    if (err) throw err;
+    console.log('Change successful!');
+  });
 }
 
-db.run(query, function (err) {
-  if (err) throw err;
-  console.log('Change successful!');
+
+//Query 1
+let sql = "SELECT name, party, grade_current FROM politicians WHERE party = 'R' AND (grade_current > 9 AND grade_current < 11)";
+
+db.all(sql, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("=====Query 1=====");
+  data.forEach((data) => {
+    console.log(data);
+  });
 });
+
+
+//Query 2
+
+let sql2 = "SELECT COUNT(*) as totalVotes, politicians.name FROM politicians INNER JOIN votes ON  votes.politician_id = politicians.politician_id WHERE politicians.name = 'Olympia Snowe'";
+
+db.all(sql2, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("=====Query 2=====");
+  data.forEach((data) => {
+    console.log(data);
+  });
+});
+
+
+//Query 3
+let sql3 = `SELECT politicians.name, COUNT(*) AS "totalVote" FROM politicians JOIN votes ON votes.politician_id = politicians.politician_id WHERE politicians.name LIKE "Adam %" GROUP BY  politicians.name`;
+db.all(sql3 , (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("=====Query 3=====");
+  data.forEach((data) => {
+    console.log(data);
+  });
+});
+
+//Query 4
+let sql4 = "SELECT COUNT(*) as totalVotes, politicians.name, politicians.party, politicians.location FROM votes JOIN politicians ON votes.politician_id = politicians.politician_id GROUP BY politicians.name ORDER BY COUNT(*) DESC LIMIT 3;"
+db.all(sql4, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("=====Query 4=====");
+  data.forEach((data) => {
+    console.log(data);
+  });
+});
+
+
+
+//Query 5
+let sql5 = "SELECT voters.first_name, voters.last_name, voters.gender, voters.age FROM votes INNER JOIN voters ON votes.voter_id = voters.voter_id WHERE votes.politician_id = (SELECT politicians.politician_id FROM politicians WHERE politicians.name = 'Olympia Snowe');"
+db.all(sql5, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("=====Query 5=====")
+  data.forEach((data) => {
+    console.log(data);
+  });
+});
+// close the database connection
+// db.close();
 
 
 
