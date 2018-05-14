cafc2e1e7170935b1a50c581c858f53b87510b73
Rin Ng <Rin08.ng@gmail.com>
2018-05-14 17:56:09 +0700

    Modified code for efficiency and readibility

--- setup.js
+++ setup.js
@@ -91,75 +91,63 @@ if(args[1] !== undefined){
 
 
 //Query 1
-let sql = "SELECT name, party, grade_current FROM politicians WHERE party = 'R' AND (grade_current > 9 AND grade_current < 11)";
-
-db.all(sql, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("=====Query 1=====");
-  data.forEach((data) => {
-    console.log(data);
-  });
-});
-
+let query1 = `SELECT name, party, grade_current
+              FROM politicians
+              WHERE party = 'R'
+                    AND (grade_current > 9 AND grade_current < 11);`
 
 //Query 2
 
-let sql2 = "SELECT COUNT(*) as totalVotes, politicians.name FROM politicians INNER JOIN votes ON  votes.politician_id = politicians.politician_id WHERE politicians.name = 'Olympia Snowe'";
+let query2 = `SELECT COUNT(*) as totalVotes, politicians.name
+            FROM politicians
+            INNER JOIN votes ON votes.politician_id = politicians.politician_id
+            WHERE politicians.name = 'Olympia Snowe';`
 
-db.all(sql2, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("=====Query 2=====");
-  data.forEach((data) => {
-    console.log(data);
-  });
-});
 
 
 //Query 3
-let sql3 = `SELECT politicians.name, COUNT(*) AS "totalVote" FROM politicians JOIN votes ON votes.politician_id = politicians.politician_id WHERE politicians.name LIKE "Adam %" GROUP BY  politicians.name`;
-db.all(sql3 , (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("=====Query 3=====");
-  data.forEach((data) => {
-    console.log(data);
-  });
-});
+let query3 = `SELECT politicians.name,
+            COUNT(*) AS "totalVote"
+            FROM politicians
+            JOIN votes ON votes.politician_id = politicians.politician_id
+            WHERE politicians.name LIKE "Adam %"
+            GROUP BY politicians.name;`
+
 
 //Query 4
-let sql4 = "SELECT COUNT(*) as totalVotes, politicians.name, politicians.party, politicians.location FROM votes JOIN politicians ON votes.politician_id = politicians.politician_id GROUP BY politicians.name ORDER BY COUNT(*) DESC LIMIT 3;"
-db.all(sql4, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("=====Query 4=====");
-  data.forEach((data) => {
-    console.log(data);
-  });
-});
-
-
+let query4 = `SELECT COUNT(*) as totalVotes, politicians.name, politicians.party, politicians.location
+            FROM votes
+            JOIN politicians ON votes.politician_id = politicians.politician_id
+            GROUP BY politicians.name
+            ORDER BY COUNT(*) DESC
+            LIMIT 3;`
 
 //Query 5
-let sql5 = "SELECT voters.first_name, voters.last_name, voters.gender, voters.age FROM votes INNER JOIN voters ON votes.voter_id = voters.voter_id WHERE votes.politician_id = (SELECT politicians.politician_id FROM politicians WHERE politicians.name = 'Olympia Snowe');"
-db.all(sql5, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("=====Query 5=====")
-  data.forEach((data) => {
+let query5 = `SELECT voters.first_name, voters.last_name, voters.gender, voters.age
+            FROM votes
+            INNER JOIN voters ON votes.voter_id = voters.voter_id
+            WHERE votes.politician_id = (SELECT politicians.politician_id FROM politicians
+                                         WHERE politicians.name = 'Olympia Snowe');`
+
+
+
+
+
+let arr = [query1, query2, query3, query4, query5]
+
+for(let i = 0; i < arr.length; i++){
+  db.all(arr[i], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log("========================");
+    console.log("\tQuery" + (i+1))
+    console.log("========================");
+    data = JSON.stringify(data, null, 2);
     console.log(data);
+    console.log("\n");
   });
-});
-// close the database connection
-// db.close();
-
-
+}
 
 
 
